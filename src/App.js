76db419fd40b138909fa8b64b30294ceb7a8e851
agkayster <ejike.chiboka@gmail.com>
2022-03-01 16:19:51 +0100

    added React.lazy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { Suspense } from 'react';
 
-import Investments from './components/investments/investmentIndex';
+// import Investments from './components/investments/investmentIndex';
 import InvestmentsNew from './components/investments/investmentsNew';
 import Register from './components/auth/Register';
 import Home from './components/pages/Home';
@@ -10,24 +11,30 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { faFontAwesome } from '@fortawesome/free-regular-svg-icons';
 
+const Investments = React.lazy(() =>
+	import('./components/investments/investmentIndex')
+);
+
 library.add(fas, faFontAwesome);
 function App() {
 	return (
-		<div className='container-fluid'>
-			<Router>
-				<Navbar />
-				<Routes>
-					<Route
-						path='/portfolio/InvestmentsNew'
-						element={<InvestmentsNew />}
-					/>
-					<Route path='/portfolio' element={<Investments />} />
-					<Route path='/register' element={<Register />} />
-					<Route path='/login' element={<Login />} />
-					<Route exact path='/' element={<Home />} />
-				</Routes>
-			</Router>
-		</div>
+		<Suspense fallback={<div>Loading...</div>}>
+			<div className='container-fluid'>
+				<Router>
+					<Navbar />
+					<Routes>
+						<Route
+							path='/portfolio/InvestmentsNew'
+							element={<InvestmentsNew />}
+						/>
+						<Route path='/portfolio' element={<Investments />} />
+						<Route path='/register' element={<Register />} />
+						<Route path='/login' element={<Login />} />
+						<Route exact path='/' element={<Home />} />
+					</Routes>
+				</Router>
+			</div>
+		</Suspense>
 	);
 }
 
